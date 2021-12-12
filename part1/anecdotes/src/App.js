819063c611ed5bf7b21beea220f25899e2ed8db1
Benjamin Seah <ben@benjamins-mbp.lan>
2021-12-12 22:38:58 +0800

    Complete exercises 1.12 - 1.14

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [allVotes, setAllVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [mostPopular, setMostPopular] = useState(null)
+
+  const getRandom = () => {
+    return Math.floor(Math.random() * 7)
+  }
+
+  const getMostPopular = () => {
+    const max = Math.max(...allVotes)
+    const index = allVotes.indexOf(max)
+    console.log(max)
+    return index
+  }
+
+  const incrementVote = (selected) => {
+    // increment vote
+    const arrayCopy = [...allVotes]
+    arrayCopy[selected] += 1
+    setAllVotes(arrayCopy)
+
+    // update most popular
+    const max = Math.max(...arrayCopy)
+    const index = arrayCopy.indexOf(max)
+    setMostPopular(index)
+  }
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {allVotes[selected]} votes</p>
+      <button onClick={() => incrementVote(selected)}>vote</button>
+      <button onClick={() => setSelected(getRandom)}>next anecdote</button>
+
+      <h1>Anecdote with the most votes</h1>
+      <p>{anecdotes[mostPopular]}</p>
+    </>
+  )
+}
+
+export default App
